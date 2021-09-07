3ed88dfd0be801da163f04f76930a0783b10c60d
victor-chukwu <okenwa1993@gmail.com>
2021-09-07 11:42:04 +0100

    Added modal to my page

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const ToggleBar = document.querySelector('.toggle-button');
 const body = document.querySelector('body');
 const sideBar = document.querySelector('.sidebar');
 const cancel = document.querySelector('.cancel');
+const cancelModal = document.querySelector('.cancels');
 const anchorLink = document.querySelectorAll('.anchor-link');
+const modalButton = document.querySelectorAll('.btn-modal');
+const modal = document.querySelector('.modal');
 
 function openSideBar() {
   sideBar.classList.add('show-side-bar');
@@ -14,8 +17,21 @@ function closeSideBar() {
   body.classList.remove('noscroll');
 }
 
+function openModal() {
+  modal.classList.add('show-modal');
+}
+
+function closeModal() {
+  modal.classList.remove('show-modal');
+}
+
 ToggleBar.addEventListener('click', openSideBar);
 cancel.addEventListener('click', closeSideBar);
+cancelModal.addEventListener('click', closeModal);
+
+modalButton.forEach((button) => {
+  button.addEventListener('click', openModal);
+});
 
 anchorLink.forEach((anchor) => {
   anchor.addEventListener('click', closeSideBar);
